665a24505d530f5d3ff4628ea3710740ff8f06c3
cesar <ofuchi@utfpr.edu.br>
2021-01-13 09:35:03 -0300

    time of flight functions out of the main ultrasonic file

--- @Ultrasonic/readPXI5752Xml.m
+++ @Ultrasonic/readPXI5752Xml.m
@@ -1,39 +1,45 @@
-function usObj = readPXI5752Xml(usObj,file)
+%%
+% function to read xml parameters
 
-    [filepath,name,ext] = fileparts(file);
+%%
+function usObj=readPXI5752Xml(usObj,file)
 
-    fid = fopen(file);
-    f = fread(fid,'*char')';
-    fclose(fid);
+[filepath,name,ext]=fileparts(file);
+fid=fopen(file);
+f=fread(fid,'*char')';
+fclose(fid);
+f = regexprep(f,'á','a');
+f = regexprep(f,'ê','e');
+fid  = fopen(file,'w');
+fprintf(fid,'%s',f);
+fclose(fid);
 
-    f = regexprep(f,'á','a');
-    f = regexprep(f,'ê','e');
-    fid = fopen(file,'w');
-    fprintf(fid,'%s',f);
-    fclose(fid);
+mystruct=util.xml2struct2(file);
+% initiate a object
+usObj=sensor.Ultrasonic;
+usObj.filename=name;
+usObj.filepath=filepath;
+usObj.fs=0;
+usObj.fprf=0;
+usObj.nwaves=0;
+%
+for i=1:size(mystruct.LVData.I32, 2)
+    a=cell2mat(mystruct.LVData.I32(i));
+    if strfind(a.Name.Text, 'Number of Samples')
+        usObj.samples=str2num(a.Val.Text);
+    elseif strfind(a.Name.Text, 'Number of Waves')
+        usObj.nwaves=str2num(a.Val.Text);
+    elseif strfind(a.Name.Text, 'Transducer Frequency')
+        usObj.fc=str2num(a.Val.Text);
+    elseif strfind(a.Name.Text, 'Pulse Repetition Frequency')
+        usObj.fprf=str2num(a.Val.Text);  
+    elseif strfind(a.Name.Text, 'sampling frequency')
+        usObj.fs=str2num(a.Val.Text);  
+     elseif strfind(a.Name.Text, 'sampling')
+        usObj.fs=str2num(a.Val.Text);  
+    end  
+   
+end
 
-    mystruct = util.xml2struct2(file);
-
-    % initiate a object
-    usObj = sensor.Ultrasonic;
-    usObj.filename = name;
-    usObj.filepath = filepath;
-    usObj.fs = 0;
-    usObj.fprf = 0;
-    usObj.nwaves = 0;
-
-    for i = 1 : size(mystruct.LVData.I32, 2)
-        a = cell2mat(mystruct.LVData.I32(i));
-
-        if strfind(a.Name.Text, 'Number')
-            usObj.nwaves = str2num(a.Val.Text);
-        elseif strfind(a.Name.Text, 'Pulse')
-            usObj.fprf = str2num(a.Val.Text);  
-        elseif strfind(a.Name.Text, 'Frequencia')
-            usObj.fs = str2num(a.Val.Text);  
-        elseif strfind(a.Name.Text, 'sampling')
-            usObj.fs = str2num(a.Val.Text);  
-        end  
-    end
 end
 
